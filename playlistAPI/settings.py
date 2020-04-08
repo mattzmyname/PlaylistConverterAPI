bd1e9b6102721217c8018cee839a638dccd64851
Matt Zhou <mattzmyname@gmail.com>
2020-04-08 01:50:24 -0400

    [matt] this should work now

--- playlistAPI/settings.py
+++ playlistAPI/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['http://playlistconverter-env-1.eba-q6xmiane.us-east-1.elasticbeanstalk.com/']
 
 # Application definition
 
